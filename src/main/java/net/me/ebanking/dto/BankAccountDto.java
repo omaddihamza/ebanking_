2a24649af771c87910d8d1f60dbcb5a84cf5a9de
hamza omaddi <hamza.omaddi@gmail.com>
2025-02-10 23:34:55 +0100

    added dto

---
+++ src/main/java/net/me/ebanking/dto/BankAccountDto.java
@@ -0,0 +1,4 @@
+package net.me.ebanking.dto;
+
+public class BankAccountDto {
+}
--- src/main/java/net/me/ebanking/dto/CustomerDto.java
+++ src/main/java/net/me/ebanking/dto/CustomerDto.java
@@ -1,4 +1,16 @@
 package net.me.ebanking.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDto {
+    private Long id;
+    private String name;
+    private String email;
 }

aed40ab265a060fb66e273453d7ec8ef9038ed5f
hamza omaddi <hamza.omaddi@gmail.com>
2025-02-24 15:17:21 +0000

    commit ebanking application

--- src/main/java/net/me/ebanking/EbankingApplication.java
+++ src/main/java/net/me/ebanking/EbankingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EbankingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EbankingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EbankingApplication.class, args);
+    }
 }

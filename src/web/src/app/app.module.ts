ce5c0a3c064b882b051c8d0d901b9342c1696d04
hamza omaddi <hamza.omaddi@gmail.com>
2025-02-27 20:16:23 +0000

    added font end

---
+++ src/web/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { NewCustomerComponent } from './components/new-customer/new-customer.component';
+import { DebitComponent } from './components/debit/debit.component';
+import { CreditComponent } from './components/credit/credit.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CustomersComponent,
+    AccountsComponent,
+    NewCustomerComponent,
+    DebitComponent,
+    CreditComponent,
+    TransferComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

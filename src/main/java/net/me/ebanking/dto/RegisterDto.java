b42e19871a59ae099502b40971c9de09936e1c3e
hamza omaddi <hamza.omaddi@gmail.com>
2025-03-01 19:38:56 +0000

    added RegisterDto

---
+++ src/main/java/net/me/ebanking/dto/RegisterDto.java
@@ -0,0 +1,13 @@
+package net.me.ebanking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class RegisterDto {
+
+    private String username;
+    private String password;
+
+}

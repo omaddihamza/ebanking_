c42a2fa6eb7d7c989ed0a92a86249cd92500980d
hamza omaddi <hamza.omaddi@gmail.com>
2025-03-01 19:37:14 +0000

    added role and user

---
+++ src/main/java/net/me/ebanking/entitie/Role.java
@@ -0,0 +1,19 @@
+package net.me.ebanking.entitie;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter @Getter @AllArgsConstructor @NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/net/me/ebanking/entitie/UserEntity.java
@@ -0,0 +1,26 @@
+package net.me.ebanking.entitie;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Setter @Getter @AllArgsConstructor @NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
+}

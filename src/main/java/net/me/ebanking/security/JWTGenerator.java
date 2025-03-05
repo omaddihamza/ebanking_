f38a60a6d3fc697c2af80a7055b1633b975f6679
hamza omaddi <hamza.omaddi@gmail.com>
2025-03-05 23:23:12 +0000

    JWT generator

---
+++ src/main/java/net/me/ebanking/security/JWTGenerator.java
@@ -0,0 +1,42 @@
+package net.me.ebanking.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+
+import java.util.Date;
+
+public class JWTGenerator {
+    public  String generateToke(Authentication authentication){
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.ES512, SecurityConstants.JWT_SECRET).compact();
+
+        return token;
+    }
+
+    public String getUsernameFromJWT(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public Boolean validateToken(String token){
+        try {
+            Jwts.parser().setSigningKey(SecurityConstants.JWT_SECRET).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            throw new AuthenticationCredentialsNotFoundException("JWT was expired or incorrect");
+        }
+    }
+}

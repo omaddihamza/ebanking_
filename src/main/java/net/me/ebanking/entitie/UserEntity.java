59ed5c28790ad4c03776a9fd3b55e8656662dcf4
hamza omaddi <hamza.omaddi@gmail.com>
2025-03-09 23:21:17 +0000

    modified UserEntity

--- src/main/java/net/me/ebanking/entitie/UserEntity.java
+++ src/main/java/net/me/ebanking/entitie/UserEntity.java
@@ -1,16 +1,19 @@
 package net.me.ebanking.entitie;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.List;
+import java.util.Collection;
+
+import java.util.stream.Collectors;
+
+@Data
 
 @Entity
-@Setter @Getter @AllArgsConstructor @NoArgsConstructor
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,9 +21,44 @@ public class UserEntity {
     @Column(unique = true)
     private String username;
 
-    private String password;
+    private String password; // Must be annotated with @Column
 
     @ManyToMany(fetch = FetchType.EAGER)
-    private List<Role> roles;
+    private Collection<Role> roles;
 
+    // Required methods from UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }

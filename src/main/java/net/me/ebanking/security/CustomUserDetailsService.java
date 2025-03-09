6469d8e6110d953066ca142b100b7e635e140668
hamza omaddi <hamza.omaddi@gmail.com>
2025-03-09 23:25:16 +0000

    modified User details service

--- src/main/java/net/me/ebanking/security/CustomUserDetailsService.java
+++ src/main/java/net/me/ebanking/security/CustomUserDetailsService.java
@@ -1,34 +1,25 @@
 package net.me.ebanking.security;
 
-import net.me.ebanking.entitie.Role;
+import lombok.AllArgsConstructor;
 import net.me.ebanking.entitie.UserEntity;
 import net.me.ebanking.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
+@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
     private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity user = userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("username not found") );
-        return new User(user.getUsername(), user.getPassword(), mapRoleToAuthorities(user.getRoles()));
+
+        return new User(user.getUsername(), user.getPassword(),user.getAuthorities());
     }
 
-    private Collection<GrantedAuthority> mapRoleToAuthorities(List<Role> roles){
-       return roles.stream().map(r-> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
-    }
 }

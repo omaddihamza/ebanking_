6d31c969fcf5ce64a171224951548b397c079fe2
hamza omaddi <hamza.omaddi@gmail.com>
2025-03-09 23:22:28 +0000

    added AuthResponseDTO

---
+++ src/main/java/net/me/ebanking/dto/AuthResponseDTO.java
@@ -0,0 +1,12 @@
+package net.me.ebanking.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDTO {
+    private String accessToken;
+    private String tokenType = "Bearer";
+    public AuthResponseDTO(String accessToken){
+        this.accessToken = accessToken;
+    }
+}
